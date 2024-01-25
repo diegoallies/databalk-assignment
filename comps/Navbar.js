200fdc4c2d4bc485dd2a6113a8bc83de9ab77790
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-01-25 11:08:12 +0200

    login navbar
    login navbar

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,7 +1,25 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import { useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
+  const handleLogout = () => {
+    if (typeof window !== 'undefined') {
+      // Perform the local storage clearing in a useEffect to avoid hydration errors
+      localStorage.removeItem('token');
+      localStorage.removeItem('userEmail');
+      localStorage.removeItem('username');
+      localStorage.removeItem('userId');
+      
+      // Redirect after ensuring local storage is cleared
+      // window.location.href = '/login';
+    }
+  };
+
+  useEffect(() => {
+    // Your logic for detecting the logout condition and redirecting if needed
+  }, []); // An empty dependency array will run this effect on initial component mount
+
   return (
     <nav>
       <div className="logo">
@@ -9,9 +27,9 @@ const Navbar = () => {
       </div>
       <Link href="/dashboard"><a>Dashboard</a></Link>
       <Link href="/profile"><a>Profile</a></Link>
-      <Link href="/login"><a>logout</a></Link>
+      <Link onClick={handleLogout} href="/login" style={{ cursor: 'pointer' }}>Logout</Link>
     </nav>
   );
-}
- 
+};
+
 export default Navbar;
